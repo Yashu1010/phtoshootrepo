c1d0aa51a737b58b09dc77bf07c00af97aca5125
Yashu1010 <127645247+Yashu1010@users.noreply.github.com>
2024-04-29 17:21:08 +0530

    Update delimg.php

--- delimg.php
+++ delimg.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_REQUEST['id'];
-$con=mysqli_connect('localhost','root','','skstudiodb');
+$con=mysqli_connect('yashmysqlserver.mysql.database.azure.com','yash0101','gandhi2002@!','skstudiodb');
 $del="DELETE FROM `gallary` WHERE `img_id`='$id'";
 
 $del1=mysqli_query($con,$del);
